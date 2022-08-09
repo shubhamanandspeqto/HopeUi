08b7a2d13bea9ba931e11dee3a4ce93086e543db
sumitkumar-speqto <sumitkumar@speqto.com>
2022-08-09 12:29:44 +0530

    folder

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='d-flex w-100 justify-content-center'>hello</h1>
     </div>
   );
 }
