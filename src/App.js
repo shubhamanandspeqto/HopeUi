52972020a003b89553aedec6649677fa1b525f94
Piyush0125 <piyush.chandra@speqto.com>
2022-08-09 19:08:45 +0530

    bedrock page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import './App.css';
+import "./App.css";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Login from './components/login/Login';
-import Dashboard from './components/admin/Dashboard';
-import Login from './components/login/Login';
+import Dashboard from "./components/admin/Dashboard";
+import Login from "./components/login/Login";
+import Bedrock from "./components/Bedrock/Bedrock";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />}>
-          <Route path='/about' element={<Login />} />
+        <Route path="/" element={<Dashboard />}>
+          <Route path="/about" element={<Login />} />
+          <Route path="/bedrock" element={<Bedrock />} />
         </Route>
       </Routes>
     </BrowserRouter>
