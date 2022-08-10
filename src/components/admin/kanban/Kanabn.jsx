2e08f4733fea461b2fbe03534a2a256afaf63737
sumitkumar-speqto <sumitkumar@speqto.com>
2022-08-10 16:23:16 +0530

    kanban

--- src/components/admin/kanban/Kanabn.jsx
+++ src/components/admin/kanban/Kanabn.jsx
@@ -27,44 +27,118 @@ export default function Kanabn() {
                 <div className='d-flex gap-3'>
                     {/* <img src="" alt="" /> */}
 
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <div className="dropdown">
+                        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                             Sort by:
                         </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a className="dropdown-item" href="#">Action</a></li>
+                            <li><a className="dropdown-item" href="#">Another action</a></li>
+                            <li><a className="dropdown-item" href="#">Something else here</a></li>
                         </ul>
                     </div>
 
                     <span>Group by:</span>
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <div className="dropdown">
+                        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                             Sort by:
                         </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a className="dropdown-item" href="#">Action</a></li>
+                            <li><a className="dropdown-item" href="#">Another action</a></li>
+                            <li><a className="dropdown-item" href="#">Something else here</a></li>
                         </ul>
                     </div>
 
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <div className="dropdown">
+                        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                             <img src="/assets/Send.png" alt="" className='px-2' />
                             Share:
                         </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a className="dropdown-item" href="#">Action</a></li>
+                            <li><a className="dropdown-item" href="#">Another action</a></li>
+                            <li><a className="dropdown-item" href="#">Something else here</a></li>
                         </ul>
                     </div>
                 </div>
             </div>
 
+            <div className='kanban-body row m-0 p-3'>
+                <div className='col-md-3 gap-4 d-flex flex-column'>
+                    <div className='kanban-heading d-flex p-3 justify-content-between'>
+                        <p className='kanban-open-projects'>Open Projects  05</p>
+                        <img src="/assets/menuDots.png" alt="" />
+                    </div>
 
+                    <div className='kanban-new-task d-flex p-3 justify-content-between align-items-center'>
+                        <p>New Task</p>
+                        <img src="/assets/PlusIcon.png" alt="" />
+                    </div>
+
+                    <div className='kanban-card p-3'>
+                        <div className='d-flex justify-content-between align-items-center'>
+                            <nav className='breadcrumb-container' aria-label="breadcrumb">
+                                <ol className="breadcrumb m-0 p-0">
+                                    <li className="breadcrumb-item active breadcrumb-item-css" aria-current="page">Dashboard</li>
+                                    <li className="breadcrumb-item active breadcrumb-item-css" aria-current="page">List</li>
+                                </ol>
+                            </nav>
+                            <img src="/assets/menuDots.png" alt="" />
+                        </div>
+
+                        <p className='mt-3'>Create Sidebar in Dashboard</p>
+
+                        <div className='d-flex kanban-card-icons align-items-center gap-2 my-2'>
+                            <div><img src="/assets/flagIcon.png" alt="" /></div>
+                            <div><img src="/assets/calenderIcon.png" alt="" /></div>
+                            <div><img src="/assets/editIcon.png" alt="" /></div>
+                            <div><img src="/assets/dropdownIcon.png" alt="" /></div>
+                        </div>
+
+                        <div className='d-flex kanban-card-add-menu align-items-center mt-4 mb-2'>
+                            <div>AT</div>
+                            <div>WE</div>
+                            <div>GK</div>
+                            <div><img src="/assets/PlusIcon.png" alt="" /></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-md-3 gap-4 d-flex flex-column'>
+                    <div className='kanban-heading d-flex p-3 justify-content-between'>
+                        <p className='kanban-progress-projects'>In Progress  02</p>
+                        <img src="/assets/menuDots.png" alt="" />
+                    </div>
+
+                    <div className='kanban-new-task d-flex p-3 justify-content-between align-items-center'>
+                        <p>New Task</p>
+                        <img src="/assets/PlusIcon.png" alt="" />
+                    </div>
+                </div>
+
+                <div className='col-md-3 gap-4 d-flex flex-column'>
+                    <div className='kanban-heading d-flex p-3 justify-content-between'>
+                        <p className='kanban-reviews-projects'>Reviews  01</p>
+                        <img src="/assets/menuDots.png" alt="" />
+                    </div>
+                    <div className='kanban-new-task d-flex p-3 justify-content-between align-items-center'>
+                        <p>New Task</p>
+                        <img src="/assets/PlusIcon.png" alt="" />
+                    </div>
+                </div>
+
+                <div className='col-md-3 gap-4 d-flex flex-column'>
+                    <div className='kanban-heading d-flex p-3 justify-content-between'>
+                        <p className='kanban-closed-projects'>Closed  03</p>
+                        <img src="/assets/menuDots.png" alt="" />
+                    </div>
+                    <div className='kanban-new-task d-flex p-3 justify-content-between align-items-center'>
+                        <p>New Task</p>
+                        <img src="/assets/PlusIcon.png" alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
