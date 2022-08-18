8bdd7de2fc7131d893e90ba6ebe98c55aa0bf3e3
sumitkumar-speqto <sumitkumar@speqto.com>
2022-08-18 13:34:38 +0530

    merge

--- src/components/admin/DashboardHome/DashboardHome.jsx
+++ src/components/admin/DashboardHome/DashboardHome.jsx
@@ -6,6 +6,69 @@ import "react-circular-progressbar/dist/styles.css";
 
 import Chart from "react-apexcharts";
 
+let barChartData = {
+
+    series: [{
+        name: 'PRODUCT A',
+        data: [44, 55, 41, 67, 22, 43]
+    }, {
+        name: 'PRODUCT B',
+        data: [13, 23, 20, 8, 13, 27]
+    }],
+    options: {
+        chart: {
+            type: 'bar',
+            height: 350,
+            stacked: true,
+            toolbar: {
+                show: true
+            },
+            zoom: {
+                enabled: true
+            }
+        },
+        responsive: [{
+            breakpoint: 480,
+            options: {
+                legend: {
+                    position: 'bottom',
+                    offsetX: -10,
+                    offsetY: 0
+                }
+            }
+        }],
+        plotOptions: {
+            bar: {
+                horizontal: false,
+                borderRadius: 10
+            },
+            radialBar: {
+                dataLabels: {
+                    show: false
+                }
+            }
+        },
+        xaxis: {
+            type: 'datetime',
+            labels: {
+                format: "ddd"
+            },
+            categories: ['01/01/2011 GMT', '01/02/2011 GMT', '01/03/2011 GMT', '01/04/2011 GMT',
+                '01/05/2011 GMT', '01/06/2011 GMT'
+            ],
+        },
+        legend: {
+            position: 'right',
+            offsetY: 40
+        },
+        fill: {
+            opacity: 1
+        }
+    },
+
+
+};
+
 let donutData = {
     options: {
         labels: ["Fashion", "Accessories"],
@@ -341,7 +404,11 @@ export default function DashboardHome() {
                             </div>
                         </div>
 
-                        <div className='col-md-7'>
+                        <div className='col-md-7 px-3'>
+
+                            <div className='w-100 dashboard-bar-graph'>
+                                <Chart options={barChartData.options} series={barChartData.series} type="bar" />
+                            </div>
                         </div>
                     </div>
                 </div>
