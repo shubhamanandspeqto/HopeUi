c89d46aa30679ee2a2158b92813e6680bc100d2a
vikas-speqto <sourav.rout@speqto.com>
2022-08-23 10:39:29 +0530

    navbar changes

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-// import logo from "../../../public/assets/logo.png";
-// import messages3 from "../Assets/messages-3.jpg";
-// import messages2 from "../Assets/messages-2.jpg";
-// import messages1 from "../Assets/messages-1.jpg";
-// import profileImg from "../Assets/profile-img.jpg";
+
 
 
 export default function Navbar() {
@@ -25,13 +21,15 @@ export default function Navbar() {
               
                     </form>
                 </div>
-                <div className='col-md-3'>
+                <div className='col-md-3 d-flex align-items-center gap-2'>
                     <img src={"/assets/flag.png"} alt="flag" />
                     <img src={"/assets/Notification.png"} alt="notification" />
                     <img src={"/assets/Message.png"} alt="message" />
-                    <img src={"/assets/User.png"} alt="user" />
-
-
+                    <img src={"/assets/Navbar-user.png"} alt="user" />
+                    <div className="user-profile">
+                      <h5>Austin Robertson</h5> 
+                      <p className="d-flex text-grey">Marketing Administrator</p>
+                        </div>
                 </div>
                 </div>
             </div>
