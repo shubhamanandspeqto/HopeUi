74af6680c714883989fb7cfe5740ab0adf1e873b
sumitkumar-speqto <sumitkumar@speqto.com>
2022-08-24 16:48:12 +0530

    client changes

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -7,32 +7,31 @@ export default function Navbar() {
         <nav className="navbar fixed-top bg-light dark-mode">
             <div className="container-fluid">
                 <div className='row w-100 dark-mode'>
-                <div className='col-md-2 '>
-                    <a className="navbar-brand dark-mode" href="#" >
-                        <img src={"/assets/logo.png"} alt="" width="30" height="24" className="d-inline-block align-text-top" />
-                        Hope Ui
-                    </a>
-                    <img src={"/assets/leftArrow.png"} />
-                </div>
+                    <div className='col-md-2 d-flex justify-content-between align-items-center'>
+                        <a className="navbar-brand dark-mode" href="#" >
+                            Bedrock
+                        </a>
+                        <img src={"/assets/leftArrow.png"} />
+                    </div>
 
-                <div className='col-md-6'>
-                    <form className="d-flex w-50" role="search">
-                        <input className="form-control dark-mode me-2" type="search" placeholder="Search..." aria-label="Search" />
-              
-                    </form>
-                </div>
-                <div className='col-md-4 d-flex align-items-center gap-2'>
-                    <img src={"/assets/flag.png"} alt="flag" />
-                    <img src={"/assets/Notification.png"} alt="notification" />
-                    <img src={"/assets/Message.png"} alt="message" />
-                    <img src={"/assets/Navbar-user.png"} alt="user" />
-                    <div className="user-profile d-flex flex-column">
-                      <h5 className='p-0 m-0'>Austin Robertson</h5> 
-                      <p className=""
-                      style={{color:"#8a92a6"}}
-                      >Marketing Administrator</p>
+                    <div className='col-md-6 d-flex align-items-center'>
+                        <form className="d-flex w-50" role="search">
+                            <input className="form-control dark-mode me-2" type="search" placeholder="Search..." aria-label="Search" />
+
+                        </form>
+                    </div>
+                    <div className='col-md-4 d-flex align-items-center gap-2'>
+                        <img src={"/assets/flag.png"} alt="flag" />
+                        <img src={"/assets/Notification.png"} alt="notification" />
+                        <img src={"/assets/Message.png"} alt="message" />
+                        <img src={"/assets/Navbar-user.png"} alt="user" />
+                        <div className="user-profile d-flex flex-column">
+                            <h5 className='p-0 m-0'>Admin - User</h5>
+                            <p className=""
+                                style={{ color: "#8a92a6" }}
+                            >Marketing Administrator</p>
                         </div>
-                </div>
+                    </div>
                 </div>
             </div>
         </nav>
